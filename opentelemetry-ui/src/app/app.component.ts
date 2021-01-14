3d606073a1eb2c07d445964098d8c769c329376c
Pascal Betting <pascal.betting@gmx.de>
2021-01-14 17:44:12 +0100

    chore: add sample UI app content with http request

--- opentelemetry-ui/src/app/app.component.ts
+++ opentelemetry-ui/src/app/app.component.ts
@@ -1,10 +1,19 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { Observable, of } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'opentelemetry-ui';
+  todos$: Observable<Object> = of({});
+
+  constructor(private readonly http: HttpClient) {}
+
+  getTodos(): void {
+    const url = 'https://jsonplaceholder.typicode.com/todos';
+    this.todos$ = this.http.get(url);
+  }
 }
