815d23c0f758cbe99ac2b2c175756926908f01d8
Pascal Betting <pascal.betting@gmx.de>
2021-01-19 21:48:28 +0100

    feat(backend): make trace context propagation possible by specifying the propagation type

--- startBackend.sh
+++ startBackend.sh
@@ -9,10 +9,12 @@ cp ./opentelemetry-backend-spring-boot/target/*.jar ./dist/app.jar
 export OTEL_INSTRUMENTATION_JAR="https://github.com/open-telemetry/opentelemetry-java-instrumentation/releases/download/v0.13.0/opentelemetry-javaagent-all.jar"
 wget $OTEL_INSTRUMENTATION_JAR -O ./dist/instrumentation.jar
 
-
 # export directly to jaeger
 export OTEL_EXPORTER=jaeger
 export OTEL_EXPORTER_JAEGER_SERVICE_NAME=opentelemetry-backend
+
+# set propagation specification
+export OTEL_PROPAGATORS=b3
 
 # start application
 java -javaagent:./dist/instrumentation.jar \
